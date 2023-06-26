aac3e2d329905f8ab5a53914c8b7e44c81804725
Archana Vaidheeswaran <varchanaiyer139@gmail.com>
2023-06-26 10:22:55 +0800

    Update cloud_services.py

--- backend/cloud_services.py
+++ backend/cloud_services.py
@@ -1,5 +1,16 @@
 import boto3
 
+s3 = boto3.resource("s3")
+bucket = s3.create_bucket(Bucket="my_weather_app_bucket")
+
 def upload_to_s3(file_name, bucket, object_name=None):
-    # Upload code here
-    pass
+    if object_name is None:
+        object_name = file_name
+
+    s3_client = boto3.client("s3")
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
